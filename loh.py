c319c5ebadb89c9e61040ba52d87713e75a43c6d
ElishaMarks <ironbasedalloy@gmail.com>
2024-03-13 03:05:08 +0200

    Rewrite camera functionality for rpicam

--- loh.py
+++ loh.py
@@ -2,22 +2,28 @@ import cv2
 import os
 import subprocess as sp
 from ultralytics import YOLO
+from picamera2 import Picamera2, Preview
 
 # Path to the YOLO model file
 model_path = os.path.join('.', 'runs', 'detect', 'train7', 'weights', 'last.pt')
 
+# Create camera context
+picam2 = Picamera2()
+#picam2.configure(picam2.create_preview_configuration({"size": (640, 480)}))
+#picam2.start_preview(Preview.QTGL)
+picam2.start()
+
 # Create the YOLO object
 model = YOLO(model_path)
+
 # Capture video from the webcam
-cap = cv2.VideoCapture(0)
 proc = None
 while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-    print("this is a loop")
+    frame = picam2.capture_array()
+    frame = frame[:,:,:3]
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     # Downsample the frame
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR);
+    #frame = cv2.resize(frame, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_LINEAR);
     
     # Define the FFmpeg command
     command = [
@@ -27,7 +33,8 @@ while True:
         '-vcodec','rawvideo',
         '-pix_fmt', 'bgr24',
         '-s', '{}x{}'.format(*frame.shape[1::-1]),
-        '-r', str(cap.get(cv2.CAP_PROP_FPS)),
+        #'-r', str(cap.get(cv2.CAP_PROP_FPS)),
+        '-r', '15',
         '-i', '-',
         '-an',
         '-vcodec', 'mpeg4',
@@ -46,7 +53,7 @@ while True:
     for result in results.boxes.data.tolist():
         x1, y1, x2, y2, score, class_id = result
 
-        if score > 0.5:
+        if score > 0.65:
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
             label = f"{results.names[int(class_id)].upper()} : {score:.2f}"
             cv2.putText(frame, label, (int(x1), int(y1 - 10)),
@@ -60,5 +67,5 @@ while True:
         break
 
 # Release the webcam
-cap.release()
-cv2.destroyAllWindows()
+#cap.release()
+#cv2.destroyAllWindows()
