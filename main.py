a9d2f60d2b3f9b20b1a7e99598d73f802abe9977
Vlad Rusu <rusu.v2k@gmail.com>
2024-03-20 03:17:45 +0200

    Change training values in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO("/home/heiwa/git/tomat/T.O.M.A.T.O.S./runs/detect/train7/weights/last.pt")
-results = model.train(data="config.yaml", epochs=3, device=0)
+results = model.train(data="config.yaml", epochs=300, device=0, resume=True)
